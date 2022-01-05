c246abc990f8be6818ae8f86769934e5201c4aa5
wwsciffsww <amt15@rice.edu>
2022-01-05 17:51:31 -0500

    def work

--- main.js
+++ main.js
@@ -2,29 +2,15 @@ import { findDistance, findPath, getEveryone } from "./geneology.js"
 
 var peopleList = document.getElementById("people")
 
-
-var action = document.getElementById("action").value
-var person1 = document.getElementsByName("person1name").value
-var person2 = document.getElementsByName("person2name").value
-var submit = document.getElementById("submit")
-
 function doStuff() {
-    console.log(action)
-    console.log(person1)
-    console.log(person2)
-    console.log("Pressed")
 
-    document.getElementById("person1display").innerHTML = person1
+    var action = document.getElementById("action").value
+    var person1 = document.getElementById("person1name").value
+    var person2 = document.getElementById("person2name").value
+  
+    console.log([action, person1, person2])
+    document.getElementById("person1display").innerHTML = action + " from " + person1 + " to " + person2
     document.getElementById("person1display").disabled = false
-
-    document.getElementById("person2display").innerHTML = person2
-    document.getElementById("person2display").disabled = false
-
-    document.getElementById("actiondisplay").innerHTML = action
-    document.getElementById("actiondisplay").disabled = false
-
-    document.getElementById("pathdisplay").innerHTML = findPath("Alan Tapper", "Kian Robinson")
-    document.getElementById("pathdisplay").disabled = false
-}
+  }
 
 //console.log(findPath("Alan Tapper", "Kian Robinson"))
